6194bd2bf305fb5639d9ee3e19aed57c42e4b7fe
Roberto Soares <roberto.melo@businessintegrator.com.br>
2023-03-31 15:20:16 -0300

    Ajustes em rota de teste

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+});
+app.listen(process.env.PORT || 3000);
