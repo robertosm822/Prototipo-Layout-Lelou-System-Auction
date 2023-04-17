91e01bdb4c5520748cc0598e7f683dc2b0645bbb
Roberto Soares <roberto.melo@businessintegrator.com.br>
2023-04-16 21:26:28 -0300

    Subindo versao de pagina 404

--- routes/adminRouters.js
+++ routes/adminRouters.js
@@ -3,20 +3,23 @@ const  router = express.Router();
 
 router.get('/', async  function(req, res, next) {
     let  data = {
-      message: '',
       layout:  'layout.njk',
-      title: ''
     }  
     res.render('admin/dashboard.njk', data);
 });
 
 router.get('/dashboard', async  function(req, res, next) {
     let  data = {
-      message: '',
       layout:  'layout.njk',
-      title: ''
     }  
     res.render('admin/dashboard.njk', data);
+});
+
+router.get('/profile', async  function(req, res, next) {
+    let  data = {
+      layout:  'layout.njk',
+    }  
+    res.render('admin/profile.njk', data);
 });
 
 export default router;
